a55adfc16b51979e174834207478c728e4068926
Parth Patel <parthpatelgee@gmail.com>
2017-02-09 17:31:25 +0000

    uses for each instead of for

--- waldo.js
+++ waldo.js
@@ -1,15 +1,21 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(name, index){
+  //for (var i = 0; i < arr.length; i++) {
+    if (name === "Waldo") {
+      found(index);   // execute callback
       //console.log(arr.indexOf("Waldo"));
     }
-  }
+  });
 }
 
 function actionWhenFound(here) {
   console.log("Found him!" + here) /*arr.indexOf("Waldo")*/
 }
 
+/*function anotherActionWhenFound(here) {
+
+}*/
+
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+/*findWaldo(["Alice", "Bob", "Waldo", "Winston"], anotherActionWhenFound);*/
